39224012206d2e7e08bd7c07c0a7f2078e2cd997
hoanghoatham03 <153010793+hoanghoatham03@users.noreply.github.com>
2024-06-19 21:27:15 +0700

    Add files via upload

---
+++ Cau_1_2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('This is a next level SEO app')
+st.write('Make your idea real')
+st.write('What is your seed keyword?')
+st.text_input(label = '', value ='', label_visibility = 'hidden')
+
+# Link web: https://21522120-cau1-2.streamlit.app/
